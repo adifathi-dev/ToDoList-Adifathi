f0487eec0d197a05a3b46013f3230e22ac250f2d
adifathi-dev <komarudin.adifathi@gmail.com>
2025-09-14 09:54:30 +0700

    ToDoLIst_Adifathi
    manajeman Tugas dan Anggaran
    
    Co-Authored-By: Adi Fathi <adifathi96@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
 });
